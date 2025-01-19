0ed41cf42f786f929bc7e83e7d8e0c6f6f3ae422
Dan Cox <daniel.57@Gmail.com>
2025-01-19 12:17:11 -0600

    Adding Twine 2 HTML compilation support

---
+++ TestTwee/Twine2HTML.Compile.Test.cs
@@ -0,0 +1,48 @@
+using NUnit.Framework;
+using libtwee;
+
+namespace TestTwee
+{
+    [TestFixture]
+    public class CompileTwine2HTMLTest
+    {
+        [Test]
+        public void Compile_InvalidInput_ThrowsException_StoryFormatSourceEmpty()
+        {
+            // Arrange
+            var story = new Story { Name = "Test Story" };
+            var storyFormat = new StoryFormat { Source = "" };
+
+            // Act & Assert
+            var ex = Assert.Throws<Exception>(() => Twine2HTML.Compile(story, storyFormat));
+            Assert.That(ex.Message, Is.EqualTo("ERROR: The source of the story format is empty."));
+        }
+
+        [Test]
+        public void Compile_InvalidInput_ThrowsException_StoryIFIDInvalid()
+        {
+            // Arrange
+            var story = new Story { Name = "Test Story", IFID = "invalid-ifid" };
+            var storyFormat = new StoryFormat { Source = "<html>{{STORY_DATA}}</html>" };
+
+            // Act & Assert
+            var ex = Assert.Throws<Exception>(() => Twine2HTML.Compile(story, storyFormat));
+            Assert.That(ex.Message, Is.EqualTo("ERROR: The story IFID is not a valid."));
+        }
+
+        [Test]
+        public void Compile_ValidInput_MinimumStory() {
+            // Arrange
+            var story = new Story { Name = "Test Story", IFID = "12345678-1234-5678-1234-567812345678" };
+            var storyFormat = new StoryFormat { Source = """<!DOCTYPE html><html><head><meta charset=\"utf-8\"><title>{{STORY_NAME}}</title></head><body>{{STORY_DATA}}</body></html>""" };
+
+            // Act
+            string result = Twine2HTML.Compile(story, storyFormat);
+
+            // Assert contains story name
+            Assert.That(result, Does.Contain("<tw-storydata name=\"Test Story\""));
+            // Assert contains IFID
+            Assert.That(result, Does.Contain("ifid=\"12345678-1234-5678-1234-567812345678\""));
+        }
+    }
+}
---
+++ libtwee/Twine2HTML.Compile.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace libtwee
+{
+    public partial class Twine2HTML
+    {
+        /**
+         * <summary>
+         *   Compile a story and story format and return HTML code.
+         *
+         *  (https://github.com/iftechfoundation/twine-specs/blob/master/twine-2-htmloutput-spec.md)
+         * </summary>
+         * 
+         * @param story Story object.
+         * @param storyFormat Story format object.
+         * @throws Exception If the story format source is empty.
+         * @throws Exception If the story IFID is not a valid uppercase UUIDv4.
+         * @throws Exception If the story name is empty.
+         * @return string HTML code of story + story format.
+         */
+        public static string Compile(Story story, StoryFormat storyFormat)
+        {
+            // Check if the source of the storyFormat is not empty.
+            if (string.IsNullOrEmpty(storyFormat.Source))
+            {
+                throw new Exception("ERROR: The source of the story format is empty.");
+            }
+
+            // Verify the story contains an IFID in UUIDv4 format using uppercase letters.
+            if (!IFIDRegex().IsMatch(story.IFID))
+            {
+                throw new Exception("ERROR: The story IFID is not a valid.");
+            }
+
+            // Create string data HTML from the story.
+            string storyData = story.ToTwine2HTML();
+
+            // Create a copy of the story format source with the {{STORY_DATA}} placeholder replaced with the story data.
+            string storyFormatSource = storyFormat.Source.Replace("""{{STORY_DATA}}""", storyData);
+
+            // In the new copy, replace {{STORY_NAME}} with the story name.
+            storyFormatSource = storyFormatSource.Replace("""{{STORY_NAME}}""", story.Name);
+
+            // Return the new copy with the story name and story data replaced.
+            return storyFormatSource;
+        }
+
+        [GeneratedRegex(@"^[A-F0-9]{8}-[A-F0-9]{4}-[A-F0-9]{4}-[A-F0-9]{4}-[A-F0-9]{12}$")]
+        private static partial Regex IFIDRegex();
+    }
+}
